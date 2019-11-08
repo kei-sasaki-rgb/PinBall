0a81be05057f44cb16c80c833e6e40bd984e414a
kei-sasaki-rgb <kei5656@hotmail.com>
2019-11-09 01:49:05 +0900

    PinBallプロジェクト　レッスン5の課題：得点を表示しよう
    星、雲等衝撃を与えると得点が加算されるようにしました。

---
+++ Assets/Score.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    //Text用変数
+    public GameObject scoreText;
+    //スコア計算用変数
+    private int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        score = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        scoreText.GetComponent<Text>().text = score+"point";
+    }
+}
